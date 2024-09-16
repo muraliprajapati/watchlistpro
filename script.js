7821994c1cb31f1400b65b2ac9062830e03d5acc
Murli Prajapati <git@murli.dev>
2024-09-16 20:45:40 +0530

    Add website files

---
+++ script.js
@@ -0,0 +1,7 @@
+lucide.createIcons();
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
